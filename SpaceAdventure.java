3766267cc21d8cb28cd113d9b643b1c0f74d166d
stellladeecoder <stelladeecoder@gmail.com>
2017-09-21 07:07:56 -0500

    Add files via upload
    Practicing concatenation with Strings

---
+++ SpaceAdventure.java
@@ -0,0 +1,19 @@
+
+/**
+ * Practicing Java "going on an adventure" with Colonel Samantha by creating code
+ * around her travels
+ *
+ * @author Delores Carey
+ * @version 9/21/2017
+ */
+public class SpaceAdventure
+{
+    public static void main (String [] args) 
+    {
+    // create varibles 
+    String s1="Colonel";
+    String s2="Samantha";
+    String captain= s1 + " " + s2;
+    System.out.print(captain);
+    }
+}
